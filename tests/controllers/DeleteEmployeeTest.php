3ee0bc7985ce9bf1da5041a9a6ea566385e5fae0
Alexis Luque <juan.luque.ayala@gmail.com>
2020-07-03 16:53:22 -0500

    feat: Creating test to Delete employee
    Se creo el test para validar que se elimino un objeto

---
+++ tests/controllers/DeleteEmployeeTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Http\Response;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class DeleteProfileTest extends TestCase
+{
+    /*
+    *Trait to callback the BD
+    */
+    use DatabaseMigrations;
+    
+    /**************************************************
+      *             DELETE EMPLOYEE
+      **************************************************/
+     /**
+     * @test
+     * @author Luque Ayala Juan Alexis
+     * @testdox El siguiente test se usa para crear un empleado
+     * mediante la route de  nombre deleteAnEmployee
+     * la respuesta debe ser un codigo http 201
+     *  y un json con la estructura data y un code
+     */
+    public function should_delete_an_employee(){
+        //ingresar resgitros de empleados
+        $employee = factory('App\Employee')->create();
+
+        $this->seeInDatabase('employees', $employee->toArray());
+        
+        //comprobar codigo de respuesta
+        $this->delete(route('deleteAnEmployee',['id' => $employee->id]),['Authorization' => 'PDQFWb29LPWcf0gsUJpeZksVjUSf7Jnc'])
+        ->assertResponseStatus(Response::HTTP_OK);
+        //comprobar estructura de respuesta
+        $this->seeJsonStructure([
+            'data',
+            'code',
+            'type,'
+        ]);
+
+        //validando que no se encuentre registrado en la base de datos
+        $this->notSeeInDatabase('employees', $employee->toArray());
+
+     }
+
+    
+}
